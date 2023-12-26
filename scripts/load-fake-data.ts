349609066c855eda1ee2022ce0b7f879b63a7495
James <lwinnyihtut0@gmail.com>
2023-12-26 14:38:49 +0630

    db connection

--- scripts/load-fake-data.ts
+++ scripts/load-fake-data.ts
@@ -1,4 +1,19 @@
-function loadFakeData(numUser: number = 10) {
+import { Client } from "pg";
+import { loadEnvConfig } from "@next/env";
+const projectDir = process.cwd();
+loadEnvConfig(projectDir);
+async function loadFakeData(numUser: number = 10) {
   console.log(`executing load fake data. generating ${numUser} users.`);
+  const client = new Client({
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.OSTGRES_NAME,
+    password: process.env.POSTGRES_PASSWORD,
+    port: parseInt(process.env.POSTGRES_PORT!),
+  });
+  await client.connect();
+  const res = await client.query("select 1");
+  console.log(res);
+  await client.end();
 }
 loadFakeData();
