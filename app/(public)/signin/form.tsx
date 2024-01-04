eb25f2e9a822a9e0246630810928f9eb90d5aed0
James <lwinnyihtut0@gmail.com>
2024-01-04 14:38:23 +0630

    sign in page

---
+++ app/(public)/signin/form.tsx
@@ -0,0 +1,67 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { FormEvent, useState } from "react";
+
+function Form() {
+  const router = useRouter();
+  const [username, setUserName] = useState<undefined | string>("");
+  const [password, setPassword] = useState<undefined | string>("");
+  async function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    const res = await fetch("/api/login", {
+      method: "post",
+      body: JSON.stringify({ username: username, password: password }),
+    });
+    if (res.ok) {
+      router.push("/feed");
+    } else {
+      alert("log in failed");
+    }
+  }
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className=" flex flex-col gap-2 p-5 max-w-xs w-full bg-slate-800 rounded-lg"
+    >
+      <div className=" text-center">
+        <h3 className=" font-semibold">Sign In</h3>
+      </div>
+      <div>
+        <hr className="my-3" />
+      </div>
+      <div>
+        <div className=" flex flex-col gap-2">
+          <label htmlFor="">Username</label>
+          <input
+            className=" text-black p-3 border border-slate-700 rounded-lg"
+            type="text"
+            onChange={(e) => setUserName(e.target.value)}
+            value={username}
+            id="username"
+            placeholder="Username"
+            required
+          />
+        </div>
+      </div>
+      <div className=" flex flex-col gap-2">
+        <label htmlFor="">Password</label>
+        <input
+          className=" text-black p-3 border border-slate-700 rounded-lg"
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          id="password"
+          placeholder="Password"
+          required
+        />
+      </div>
+      <button
+        type="submit"
+        className=" mt-4 bg-slate-900 p-3 rounded-lg text-white"
+      >
+        Sign In
+      </button>
+    </form>
+  );
+}
+export default Form;
---
+++ app/(public)/signin/page.tsx
@@ -0,0 +1,8 @@
+import Form from "./form";
+export default async function SignIn() {
+  return (
+    <div>
+      <Form />
+    </div>
+  );
+}
