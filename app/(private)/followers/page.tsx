c3c69faa502a9b8029b9c5e24e8969b60bc0e493
James <lwinnyihtut0@gmail.com>
2024-01-13 18:45:19 +0630

    page stubs

---
+++ app/(private)/followers/page.tsx
@@ -0,0 +1,7 @@
+export default async function Follwers() {
+  return (
+    <main>
+      <h1>Followers</h1>
+    </main>
+  );
+}
---
+++ app/(private)/following/page.tsx
@@ -0,0 +1,7 @@
+export default async function Following() {
+  return (
+    <main>
+      <h1>Following</h1>
+    </main>
+  );
+}
