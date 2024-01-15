c6eb861c96c156a7243072ea563eccebefdbc36d
James <lwinnyihtut0@gmail.com>
2024-01-15 20:13:44 +0630

    followers page ui and api

---
+++ app/(private)/followers/follower-container.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+import FollowersList from "./follower-list";
+
+function FollowerContainer() {
+  const [cnt, setCnt] = useState(1);
+
+  const pages = [];
+  for (let i = 0; i < cnt; i++) {
+    pages.push(<FollowersList index={i} />);
+  }
+  return (
+    <div>
+      {pages}
+      <div className="flex justify-center w-full">
+        <button
+          onClick={() => setCnt(cnt + 1)}
+          className=" bg-slate-900 p-2 rounded-lg"
+        >
+          Load More
+        </button>
+      </div>
+    </div>
+  );
+}
+export default FollowerContainer;
--- app/(private)/followers/page.tsx
+++ app/(private)/followers/page.tsx
@@ -1,7 +1,11 @@
+"use client";
+import FollowerContainer from "./follower-container";
+
 export default async function Follwers() {
   return (
     <main>
       <h1>Followers</h1>
+      <FollowerContainer />
     </main>
   );
 }
