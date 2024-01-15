258d1cedc017fddc87d373d7213e20dc9dae9bb7
James <lwinnyihtut0@gmail.com>
2024-01-15 11:17:36 +0630

    update post endpoint

--- app/api/posts/[id]/route.ts
+++ app/api/posts/[id]/route.ts
@@ -1,5 +1,6 @@
 import { getJWTPayload } from "@/app/util/auth";
 import { sql } from "@/db";
+import { error } from "console";
 import { NextResponse } from "next/server";
 
 export async function GET(
@@ -15,4 +16,24 @@ export async function GET(
     return NextResponse.json({ error: "not found" }, { status: 404 });
   }
   return NextResponse.json({ data: res.rows[0] });
+}
+export async function PATCH(
+  request: Request,
+  { params }: { params: { id: number } }
+) {
+  const body = await request.json();
+  const jwtPayload = await getJWTPayload();
+  const res = await sql("select * from posts where user_id = $1 and id = $2", [
+    jwtPayload.sub,
+    params.id,
+  ]);
+  if (res.rowCount == 0) {
+    return NextResponse.json({ error: "not found" }, { status: 404 });
+  }
+  await sql("update posts set content = $1 where user_id = $2 and id= $3", [
+    body.content,
+    jwtPayload.sub,
+    params.id,
+  ]);
+  return NextResponse.json({ msg: "update success" });
 }
