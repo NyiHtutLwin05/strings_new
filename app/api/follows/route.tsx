9c3aca3cedbde7f49fc5c7734f409af3e7a8a743
James <lwinnyihtut0@gmail.com>
2024-01-16 13:43:31 +0630

    create follows endpoint

--- app/api/follows/route.tsx
+++ app/api/follows/route.tsx
@@ -1,5 +1,6 @@
 import { getJWTPayload } from "@/app/util/auth";
 import { sql } from "@/db";
+
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
@@ -11,4 +12,21 @@ export async function GET(request: Request) {
     [userId, jwtPayload.sub]
   );
   return NextResponse.json({ data: res.rows });
+}
+export async function POST(request: Request) {
+  const jwtPayload = await getJWTPayload();
+  const json = await request.json();
+
+  const res = await sql(
+    "select * from follows where user_id = $1 and follower_id = $2",
+    [json.user_id, jwtPayload.sub]
+  );
+  if (res && res.rowCount! > 0) {
+    return NextResponse.json({ error: "already following" }, { status: 409 });
+  }
+  await sql("insert into follows (user_id, follower_id) values ($1, $2)", [
+    json.user_id,
+    jwtPayload.sub,
+  ]);
+  return NextResponse.json({ msg: "follow success" });
 }
